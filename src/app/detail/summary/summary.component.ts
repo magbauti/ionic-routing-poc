ec7b982edda3ba957d7b603f7d75faf1d40f0617
Magz <96066118+magbauti@users.noreply.github.com>
2024-05-08 13:54:01 -0400

    Remove explore container, update tabs and support scrolling list (#3)

--- src/app/detail/summary/summary.component.ts
+++ src/app/detail/summary/summary.component.ts
@@ -1,20 +1,27 @@
-import {Component} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {ViewWillEnter} from "@ionic/angular";
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
 })
-export class SummaryComponent  implements ViewWillEnter {
+export class SummaryComponent implements ViewWillEnter {
   id: string | null = '';
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ionViewWillEnter() {
-    console.log('summary: this.route.snapshot.parent.parent.parent.paramMap: ', this.route.snapshot.parent?.parent?.parent?.paramMap)
-    console.log('summary: this.route.snapshot.paramMap: ', this.route.snapshot.paramMap)
-    this.id = this.route.snapshot.parent?.parent?.parent?.paramMap.get('id') ?? '';//this.route.snapshot.paramMap.get('id');
+    console.log(
+      'summary: this.route.snapshot.parent.parent.parent.paramMap: ',
+      this.route.snapshot.parent?.parent?.parent?.paramMap
+    );
+    console.log(
+      'summary: this.route.snapshot.paramMap: ',
+      this.route.snapshot.paramMap
+    );
+    this.id = `${
+      this.route.snapshot.parent?.parent?.parent?.paramMap.get('id') ?? ''
+    }00`; //this.route.snapshot.paramMap.get('id');
   }
-
 }
